8ab9b5ac60a6fee6599ed07523a7ec537ecc773b
Seungwoo Kang <swkang@koreatech.ac.kr>
2018-09-14 05:53:08 +0100

    updated

--- gpio/simpleLedBlink.py
+++ gpio/simpleLedBlink.py
@@ -6,14 +6,15 @@ led_pin = 5
 gpio.setmode(gpio.BCM)
 gpio.setup(led_pin, gpio.OUT)
 
-gpio.output(led_pin, True)
-time.sleep(0.5)
-gpio.output(led_pin, False)
-time.sleep(0.5)
-gpio.output(led_pin, True)
-time.sleep(0.5)
-gpio.output(led_pin, False)
-time.sleep(0.5)
-
-print("Blink Finished")
-gpio.cleanup()
+try:
+    for i in range(0, 20):
+        print("Iteration " + str(i+1))
+        gpio.output(led_pin, True)
+        time.sleep(1)
+        gpio.output(led_pin, False)
+        time.sleep(1)
+    print("Blink Finished")
+    gpio.cleanup()
+except KeyboardInterrupt:
+    gpio.cleanup()
+    
