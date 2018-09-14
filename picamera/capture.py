effd11660716145b250bfa853768199767925cac
Seungwoo Kang <swkang@koreatech.ac.kr>
2018-09-14 07:00:00 +0100

    picamera examples updated

--- picamera/capture.py
+++ picamera/capture.py
@@ -2,12 +2,12 @@ import picamera
 import time
 
 with picamera.PiCamera() as camera: # the close() method is automatically called
-	camera.resolution = (1024, 768)
-        camera.start_preview()
-        # Camera warm-up time
-        time.sleep(2)
-        #camera.capture('foo.jpg')
-	camera.capture('foo.jpg', resize=(320,240)) # capturing resized images
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    #camera.capture('foo.jpg')
+    camera.capture('foo.jpg', resize=(320,240)) # capturing resized images
 
 #camera = picamera.PiCamera()
 #try:
--- picamera/timestampedrecording.py
+++ picamera/timestampedrecording.py
@@ -2,18 +2,18 @@ import picamera
 import datetime as dt
 
 with picamera.PiCamera() as camera:
-	camera.resolution = (1280, 720)
-	camera.framerate = 24
-	camera.start_preview()
+    camera.resolution = (1280, 720)
+    camera.framerate = 24
+    camera.start_preview()
 
-	camera.annotate_background = picamera.Color('black')
-	camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-	camera.start_recording('timestamped.h264')
+    camera.annotate_background = picamera.Color('black')
+    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    camera.start_recording('timestamped.h264')
 
-	start = dt.datetime.now()
-	while (dt.datetime.now() - start).seconds < 30:
-		camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-		camera.wait_recording(0.2)
+    start = dt.datetime.now()
+    while (dt.datetime.now() - start).seconds < 30:
+        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        camera.wait_recording(0.2)
 
-	camera.stop_recording()
-	camera.stop_preview()
+    camera.stop_recording()
+    camera.stop_preview()
